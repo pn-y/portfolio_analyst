917b252bb711e99b958b75e4f59dbc9864787a7f
Artem Skripka <therewasadrizzle@gmail.com>
2020-05-03 00:16:03 +0400

    add arc

--- mix.exs
+++ mix.exs
@@ -44,8 +44,8 @@ defmodule PortfolioAnalyst.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
       {:plug_cowboy, "~> 2.0"},
+      {:arc_ecto, "~> 0.11.3"},
       {:credo, "~> 1.4", only: [:dev, :test], runtime: false}
     ]
   end
